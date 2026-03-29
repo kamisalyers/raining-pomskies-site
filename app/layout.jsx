43838a538e8982c7420a40884d646ea7ff7271c8
kamisalyers <kami.salyers@gmail.com>
2026-03-28 23:13:57 -0500

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,10 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Raining Pomskies',
+  description: 'Quality Pomsky Puppies Raised in the Heart of Oklahoma',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
